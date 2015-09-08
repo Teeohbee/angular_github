ac6ddc2ea94f9eadaf8ebbb22efdfa449bfbd5aa
Katya-yani Vyas <katyayani.vyas@gmail.com>
2015-09-08 15:04:56 +0100

    Changed some things

--- js/GitUserSearchController.js
+++ js/GitUserSearchController.js
@@ -1,6 +1,5 @@
 githubUserSearch.controller('GitUserSearchController', ['$resource', function($resource) {
 
-  // var access_token = '81c45763ea864067f79f604c773ea9dbd849e756'
   var self = this;
   var searchResource = $resource('https://api.github.com/search/users');
   
