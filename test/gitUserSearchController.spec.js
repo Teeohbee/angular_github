a899cfabd5efefc1038c1ade47931ec15194b2b0
Toby Clarke <teeohbee@gmail.com>
2015-09-08 17:00:20 +0100

    Added factory search

--- test/gitUserSearchController.spec.js
+++ test/gitUserSearchController.spec.js
@@ -14,28 +14,30 @@ describe('GitUserSearchController', function() {
 
   describe('when searching for a user', function() {
 
-    var items = [
-      {
-        "login": "tansaku",
-        "avatar_url": "https://avatars.githubusercontent.com/u/30216?v=3",
-        "html_url": "https://github.com/tansaku"
-      },
-      {
-        "login": "stephenlloyd",
-        "avatar_url": "https://avatars.githubusercontent.com/u/196474?v=3",
-        "html_url": "https://github.com/stephenlloyd"
-      }
-    ];
+    var items = [{
+      "login": "tansaku",
+      "avatar_url": "https://avatars.githubusercontent.com/u/30216?v=3",
+      "html_url": "https://github.com/tansaku"
+    }, {
+      "login": "stephenlloyd",
+      "avatar_url": "https://avatars.githubusercontent.com/u/196474?v=3",
+      "html_url": "https://github.com/stephenlloyd"
+    }];
 
     var httpBackend;
-      beforeEach(inject(function($httpBackend) {
-        httpBackend = $httpBackend
-        httpBackend
-          .when("GET", "https://api.github.com/search/users?q=hello")
-          .respond(
-            { items: items }
-          );
-      }));
+    beforeEach(inject(function($httpBackend) {
+      httpBackend = $httpBackend
+      httpBackend
+        .expectGET("https://api.github.com/search/users?access_token=" + accessToken + "&q=hello")
+        .respond({
+          items: items
+        });
+    }));
+
+    afterEach(function() {
+      httpBackend.verifyNoOutstandingExpectation();
+      httpBackend.verifyNoOutstandingRequest();
+    });
 
     it('displays search results', function() {
       ctrl.searchTerm = 'hello';
