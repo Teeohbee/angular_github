b3b07f3d597b7fc3286f52fb78315f2a867051da
Toby Clarke <teeohbee@gmail.com>
2015-09-10 15:11:57 +0100

    Added api key in ENV

--- public/js/searchFactory.js
+++ public/js/searchFactory.js
@@ -1,13 +1,19 @@
 githubUserSearch.factory('Search', ['$http', function($http) {
 
   var queryUrl = 'https://api.github.com/search/users';
+
+  var accessToken = $http({method: 'GET', url: '/key'});
+  accessToken.then(function(success) {
+    self.token = success.data;
+  });
+
   return {
     query: function(searchTerm) {
       return $http({
         url: queryUrl,
         method: 'GET',
         params: {
-          'access_token': accessToken,
+          'access_token': self.token.access_token,
           'q': searchTerm
         }
       });
--- server.rb
+++ server.rb
@@ -2,6 +2,12 @@ require 'sinatra'
 
 # set :public_folder, proc { File.join(root) }
 
+APIKEY = ENV['GITHUB_API_SECRET']
+
 get '/' do
   send_file 'public/index.html'
+end
+
+get '/key' do
+  { access_token: APIKEY }.to_json
 end
